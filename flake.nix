5d88e8e1bba8b225e0281b48d3a29b6c352e9334
Federico Izzo <federico.izzo99@gmail.com>
2023-02-23 09:58:12 +0100

    update nix stuff

--- flake.nix
+++ flake.nix
@@ -1,19 +1,51 @@
 {
-  description = "Hugo website";
-  outputs = { self, nixpkgs }: {
+  description = "Federico Izzo personal website built with Hugo";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        flake = { };
+        systems = [ "x86_64-linux" "aarch64" ];
+        perSystem = { pkgs, lib, ... }:
+          let
+            shell = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                go
+                hugo
+              ];
+            };
+            blowfish = pkgs.fetchFromGitHub {
+              repo = "blowfish";
+              owner = "nunocoracao";
+              rev = "v2.27.0";
+              sha256 = "sha256-dN4wQC0tUynFwXyo0isF9yHPcDAQ9Ec9OH7JeF759nM=";
+            };
 
-    # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-    devShell.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          hugo
-        ];
+            fedeizzodev = pkgs.stdenv.mkDerivation {
+              name = "fedeizzodev";
+              src = ./.;
+              buildInputs = with pkgs;[ git go hugo ];
+              buildPhase = ''
+                mkdir -p themes/blowfish
+                cp -r ${blowfish}/* themes/blowfish
+                hugo
+              '';
+              installPhase = ''
+                mkdir -p $out
+                cp -r * $out/
+              '';
+            };
+          in
+          {
+            packages.default = fedeizzodev;
+            devShells.default = shell;
+          };
       };
-  };
 }
+
+
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/fedeizzo/fedeizzo.dev
-
-go 1.18
-
-require github.com/nunocoracao/blowfish v1.2.0 // indirect
